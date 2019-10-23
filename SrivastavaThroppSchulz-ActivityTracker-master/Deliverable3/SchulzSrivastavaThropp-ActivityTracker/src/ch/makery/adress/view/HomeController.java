33ea67277a75e75b4ff2d287bdebca3df3736123
throppa <42742571+throppa@users.noreply.github.com>
2019-10-23 15:34:02 -0400

    Add files via upload

---
+++ SrivastavaThroppSchulz-ActivityTracker-master/Deliverable3/SchulzSrivastavaThropp-ActivityTracker/src/ch/makery/adress/view/HomeController.java
@@ -0,0 +1,39 @@
+package ch.makery.adress.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import ch.makery.adress.MainApp;
+
+
+public class HomeController {
+    @FXML
+    private Button calories;
+    @FXML
+    private Button settings; 
+
+
+    // Reference to the main application.
+    private MainApp mainApp;
+
+    public HomeController() {
+    }
+
+    @FXML
+    private void initialize() {
+    }
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        
+        if (event.getSource() == calories)
+           mainApp.showCaloriesBurnedScene(); 
+        else if(event.getSource() == settings)
+           mainApp.showChangeDateScene(); 
+    }
+    
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+
+    }
+}
