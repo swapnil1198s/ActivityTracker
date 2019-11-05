9977b5737684a622ad6b217703d0fafdd344d5b6
schulzjm <54485340+schulzjm@users.noreply.github.com>
2019-11-05 02:02:58 +0100

    changed conflict

--- SchulzSrivastavaThropp-ActivityTracker/src/ch/makery/adress/view/ChangeAgeController.java
+++ SchulzSrivastavaThropp-ActivityTracker/src/ch/makery/adress/view/ChangeAgeController.java
@@ -4,20 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import ch.makery.adress.MainApp;
-
-
-public class ChangeAgeController {
-    @FXML
-    private Button button;
-    
-    // Reference to the main application.
-    private MainApp mainApp;
-package ch.makery.adress.view;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import ch.makery.adress.MainApp;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 
